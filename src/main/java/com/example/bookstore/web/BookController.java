9ffb3b703c7731eaa2824cb88ae665a40045d2ae
Sadikshya <sadikshyeah@gmail.com>
2026-02-04 23:22:58 +0200

    Book list show page

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,13 +1,26 @@
 package com.example.bookstore.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
+
+@Controller
 public class BookController {
 
-    @GetMapping("/index")
-    public String index() {
-        return "Welcome to the Bookstore";
+    private BookRepository repository; 
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    @RequestMapping(value= {"/", "/booklist"})
+    public String bookList(Model model) {	
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }
