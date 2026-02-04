b267d5c32f494a96803f8386eb7b9b3f2a924472
Sadikshya <sadikshyeah@gmail.com>
2026-02-04 22:50:29 +0200

    h2 database setup and created Book entity

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,7 +1,14 @@
 package com.example.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
--- src/main/java/com/example/bookstore/domain/Book.java
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -1,7 +1,16 @@
 package com.example.bookstore.domain;
 
-public class Book {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Book {
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+    
     private String title;
     private String author;
     private int publicationYear;
